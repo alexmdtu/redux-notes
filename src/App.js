bfe0c834728518dfebce8d1ff50386dc736656f7
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-21 08:49:47 +0200

    Refactored note app to use asynchronous actions with redux thunk.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
 import { useDispatch } from 'react-redux'
 import { initializeNotes } from './reducers/noteReducer'
-import noteService from './services/notes'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    noteService.getAll().then(notes =>
-      dispatch(initializeNotes(notes))
-    )
+    dispatch(initializeNotes())
   }, [dispatch])
 
   return (
