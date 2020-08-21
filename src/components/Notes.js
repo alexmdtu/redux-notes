45cc5d5df9bdf19652f107d410c22fe20a1e1258
Alexander Tu <alexander.md.tu@gmail.com>
2020-08-21 10:21:35 +0200

    Refactored use of dispatch hook in notes list to use connected component instead.

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { useDispatch } from 'react-redux'
 import { toggleImportanceOf } from '../reducers/noteReducer'
 
 const Note = ({ note, handleClick }) => {
@@ -13,17 +12,13 @@ const Note = ({ note, handleClick }) => {
 }
 
 const Notes = (props) => {
-    const dispatch = useDispatch()
-
     return (
         <ul>
             {props.notes.map(note =>
                 <Note
                     key={note.id}
                     note={note}
-                    handleClick={() =>
-                        dispatch(toggleImportanceOf(note.id))
-                    }
+                    handleClick={() => props.toggleImportanceOf(note.id)}
                 />
             )}
         </ul>
@@ -44,6 +39,13 @@ const mapStateToProps = (state) => {
     }
 }
 
-const ConnectedNotes = connect(mapStateToProps)(Notes)
+const mapDispatchToProps = {
+    toggleImportanceOf
+}
+
+const ConnectedNotes = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Notes)
 
 export default ConnectedNotes
